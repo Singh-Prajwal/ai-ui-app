cad9189caa8516816243d8508b98f50b16fc3cf4
Prajwal Singh <102660402+Singh-Prajwal@users.noreply.github.com>
2025-06-05 11:00:43 +0530

    Updated supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL?? process.env.NEXT_PUBLIC_SUPABASE_URL:"https://hguyjryhzsddnprqvnxh.supabase.co";
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY??process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhndXlqcnloenNkZG5wcnF2bnhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2MzE3MzEsImV4cCI6MjA2MzIwNzczMX0.TfM48igVKm-ooGDF_ymI6KV4kI_dbEsYUlNK2txoEhs";
 
 export const supabase = createClient(supabaseUrl, supabaseKey!);
